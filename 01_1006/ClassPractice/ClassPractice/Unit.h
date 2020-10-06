3d6298ba0dba314503e2b890a25b444cafadd3dd
n_sato <n.satou@monochrome-corp.com>
2020-10-06 14:27:14 +0900

    リファクタリングその１ 　・クラス名を変更 　・クラスの定義を別のcpp、hに移動 　・よく使うヘッダーファイルのインクルードを、Common,hに集約

---
+++ 01_1006/ClassPractice/ClassPractice/Unit.h
@@ -0,0 +1,24 @@
+
+#ifndef UNIT_H
+#define UNIT_H
+
+// Unitクラス
+class Unit
+{
+public:
+	// コンストラクタ
+	Unit();
+	// デストラクタ
+	~Unit();
+
+	int m_hp;
+	int m_str;
+	int m_mag;
+	int m_tec;
+	int m_spd;
+	int m_def;
+	int m_luc;
+	int m_mde;
+};
+
+#endif // #ifndef UNIT_H
