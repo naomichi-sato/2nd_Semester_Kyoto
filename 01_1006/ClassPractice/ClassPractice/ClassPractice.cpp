de41a161d921ce6b5fe2dffd336f82b3f72c9436
n_sato <n.satou@monochrome-corp.com>
2020-10-06 13:47:36 +0900

    リンファクタリング前、ステータスを画面に表示する処理を追加

--- 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
+++ 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
@@ -23,6 +23,14 @@ public:
 
 Fighter::Fighter()
 {
+	m_hp	= 0;
+	m_str	= 0;
+	m_mag	= 0;
+	m_tec	= 0;
+	m_spd	= 0;
+	m_def	= 0;
+	m_luc	= 0;
+	m_mde	= 0;
 }
 
 Fighter::~Fighter()
@@ -31,6 +39,16 @@ Fighter::~Fighter()
 
 int main()
 {
+	Fighter fighter;
+
+	printf("m_hp  = %d\n", fighter.m_hp);
+	printf("m_str = %d\n", fighter.m_str);
+	printf("m_mag = %d\n", fighter.m_mag);
+	printf("m_tec = %d\n", fighter.m_tec);
+	printf("m_spd = %d\n", fighter.m_spd);
+	printf("m_def = %d\n", fighter.m_def);
+	printf("m_luc = %d\n", fighter.m_luc);
+	printf("m_mde = %d\n", fighter.m_mde);
 
 	system("pause");
 	return 0;
