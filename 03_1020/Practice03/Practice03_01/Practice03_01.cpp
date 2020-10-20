877f464c8d7d7bd37ad12a909543272fcfb290f4
NOTE-SATOU\satou <naomichi.sato.1981@gmail.com>
2020-10-20 13:33:38 +0900

    基底クラスのデストラクタにvirtualをつけなければいけない理由を実証

--- 03_1020/Practice03/Practice03_01/Practice03_01.cpp
+++ 03_1020/Practice03/Practice03_01/Practice03_01.cpp
@@ -5,25 +5,51 @@
 #include "Player.h"
 #include "Enemy.h"
 
+void PrintHp( Base* target )
+{
+	if(target == nullptr)
+	{
+		return;
+	}
+	printf("hp = %d\n", target->GetHp());
+}
+
 int main()
 {
 	// 基底クラスのポインタ変数へ、継承先のクラスのアドレスを代入できる
-	Base* player = new Player();
-	Base* enemy  = new Enemy();
+//	Base* player = new Player();
+//	Base* enemy  = new Enemy();
 
-	player->SetMoveSpeed(5.0f);
-	enemy->SetHp(100);
-	if( player->CheckHit(10, 10, 20, 30) == false )
+	// つまり、どちらもBaseクラスを継承しているので、
+	// Baseのポインタ変数配列で一括管理することが出来る
+	Base* array[] = 
 	{
-		printf("当たっていません\n");
-	}
-	if (enemy->CheckHit(10, 10, 20, 30) == false)
+		new Player(),
+		new Enemy()
+	};
+	array[1]->SetHp( 100 );
+
+	// arrayを使って当たり判定をfor文で回す
+	for( int i = 0; i < 2; i++ )
 	{
-		printf("当たっていません\n");
+		if( array[i] != nullptr )
+		{
+			PrintHp( array[i] );
+			if( array[i]->CheckHit(10, 10, 20, 30) == false )
+			{
+				printf("当たってません\n");
+			}
+		}
 	}
 
-	delete player;
-	delete enemy;
+//	delete player;
+//	delete enemy;
+
+	for (int i = 0; i < 2; i++)
+	{
+		delete array[i];
+		array[i] = nullptr;
+	}
 
 	system("pause");
 	return 0;
