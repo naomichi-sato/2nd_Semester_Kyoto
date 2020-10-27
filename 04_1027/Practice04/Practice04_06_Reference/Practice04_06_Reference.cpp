10df29a62febc1570c6d496518477ca0c65d5c18
NOTE-SATOU\satou <naomichi.sato.1981@gmail.com>
2020-10-27 16:19:57 +0900

    第４回実習その６
    参照について
    書き方、作り方
    参照渡しについて

---
+++ 04_1027/Practice04/Practice04_06_Reference/Practice04_06_Reference.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+
+// 参照を引数にもつ関数
+void Init( int& rhp, int& rmp )
+{
+	rhp = 50;
+	rmp = 10;
+}
+
+int main()
+{
+	// 参照型の書き方
+	// データ型& 変数名
+	int val  = 0;
+	int& ref = val;	// intの参照型変数ref
+
+	printf("valのアドレス %x\n", &val );
+	printf("refのアドレス %x\n", &ref );
+
+	printf("valに100を代入\n");
+	val = 100;
+	printf("val = %d\n", val );
+	printf("ref = %d\n", ref );
+
+	printf("\n", ref );
+
+	printf("refに10を代入\n");
+	ref = 10;
+	printf("val = %d\n", val );
+	printf("ref = %d\n", ref );
+
+	// 参照渡し
+	// 引数に参照を持つ関数へ変数を渡すこと
+	int hp = 0;
+	int mp = 0;
+	Init( hp, mp );	// 参照渡し
+	printf("HP = %d\n", hp);
+	printf("MP = %d\n", mp);
+
+	return 0;
+}
+
+
