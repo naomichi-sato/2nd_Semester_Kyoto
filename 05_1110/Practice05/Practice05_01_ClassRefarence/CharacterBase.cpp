6bf0c070d7270a134c122bf834a0d3e3ef179c2c
NOTE-SATOU\satou <naomichi.sato.1981@gmail.com>
2020-11-10 10:36:53 +0900

    第５回実習その１
    コンストメンバ関数を組み合わせた、クラスのコンスト参照渡し実践

---
+++ 05_1110/Practice05/Practice05_01_ClassRefarence/CharacterBase.cpp
@@ -0,0 +1,17 @@
+﻿#include "CharacterBase.h"
+
+CharacterBase::CharacterBase()
+	: Hp( 10 )
+	, Mp( 8 )
+	, Str( 6 )
+	, Vit( 5 )
+	, Int( 12 )
+{
+}
+
+CharacterBase::~CharacterBase()
+{
+}
+
+
+
---
+++ 05_1110/Practice05/Practice05_01_ClassRefarence/Practice05_01_ClassRefarence.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include "CharacterBase.h"
+
+// const参照引数を持つパラメータ表示関数
+void PrintParam( const CharacterBase& character );
+
+int main()
+{
+	CharacterBase testChar;
+	PrintParam( testChar );
+
+	return 0;
+}
+
+// const参照引数を持つパラメータ表示関数
+void PrintParam( const CharacterBase& character )
+{
+	printf( "Character Parameter List\n" );
+	printf( "------------------------\n" );
+	printf( "Hp  = %3d\n", character.GetHp() );
+	printf( "Mp  = %3d\n", character.GetMp() );
+	printf( "Str = %3d\n", character.GetStr() );
+	printf( "Vit = %3d\n", character.GetVit() );
+	printf( "Int = %3d\n", character.GetInt() );
+}
+
